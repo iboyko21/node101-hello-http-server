0810760a2e178e0750d09f6437c35574998ea423
Igor Boyko <iboyko@gmail.com>
2022-05-23 17:05:52 -0700

    added server logic

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// write your code here
+// Require the dependency (the http module from Node)
+const http = require('http');
+
+// Create a server object and make sure it can send headers and send body response
+const server = http.createServer(function (req, res) {
+    // log a msg when request is received
+    console.log('Received ' + req.method + ' request for: ' + req.url );
+    // send headers
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    // send body response
+    res.end("Hello World");
+});
+
+// Add a listener so that your server will have a way to handle incoming connections.
+server.listen(8080, 'localhost', null, function () {
+    console.log('Server is listening on localhost:8080');
+});
